50ff198889f1c820795c1609a2575aabf7d432f9
Darya Volkova <anrich@bk.ru>
2022-01-13 07:32:26 +0200

    Added small class for MyButton component

--- src/components/MyButton.vue
+++ src/components/MyButton.vue
@@ -1,22 +1,48 @@
 <template>
-    <div class="button-wrapper">
-        <slot></slot>
-    </div>
+  <div :class="compClass">
+    <slot></slot>
+  </div>
 </template>
 
+<script>
+export default {
+  props: {
+    small: {
+      type: Boolean,
+      default: false,
+    },
+  },
+
+  computed: {
+    compClass() {
+    if (this.small) {
+        return "btn btn--small" 
+    } else {
+        return "btn"
+    }
+    },
+  },
+};
+</script>
+
 <style lang="scss" scoped>
-.button-wrapper {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    background: red;
-    color: black;
-    border-radius: 50%;
-    height: 40px;
-    min-width: 40px;
-    margin: 30px;
-    cursor: pointer;
-    user-select: none;
-    z-index: 1;
+.btn {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: red;
+  color: black;
+  border-radius: 50%;
+  height: 40px;
+  min-width: 40px;
+  margin: 30px;
+  cursor: pointer;
+  user-select: none;
+  z-index: 1;
+
+  &--small {
+      height: 15px;
+  min-width: 15px;
+  }
 }
 </style>
