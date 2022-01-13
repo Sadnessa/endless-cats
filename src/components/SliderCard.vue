70f33c0835dc65acff3713259c7205320fffe635
Darya Volkova <anrich@bk.ru>
2022-01-13 06:53:22 +0200

    Added slider fixes

--- src/components/SliderCard.vue
+++ src/components/SliderCard.vue
@@ -1,10 +1,12 @@
 <template>
   <div class="card-wrapper">
-    <div v-for="img in sliderImg" :key="img" class="img-wrapper" :style="compStyle">
-      <div class="loading-wrapper" v-if="img == null">
+    <div class="scroll" :style="compStyle">
+      <div v-for="img in sliderImg" :key="img" class="img-wrapper">
+        <div class="loading-wrapper" v-if="img == null">
           ....................................
+        </div>
+        <img :src="img" v-else />
       </div>
-      <img v-else :src="img" />
     </div>
   </div>
 </template>
@@ -23,10 +25,10 @@ export default {
   },
 
   computed: {
-      compStyle() {
-          return "transform: translateX(" + this.imgIndex*-100+"%)"
-      }
-  }
+    compStyle() {
+      return "transform: translateX(" + this.imgIndex * -100 + "%)";
+    },
+  },
 };
 </script>
 
@@ -37,24 +39,29 @@ export default {
   background: rgb(224, 224, 224);
   border-radius: 20px;
   overflow: hidden;
-  white-space: nowrap;
-}
+  
 
-.img-wrapper {
-    display: inline-flex;
-    justify-content: center;
-    align-items: center;
-    vertical-align: middle;
+  .scroll {
     width: 100%;
     height: 100%;
+    white-space: nowrap;
     transition: all 0.5s ease-in-out; 
 
-    img {
+    .img-wrapper {
+      display: inline-flex;
+      justify-content: center;
+      align-items: center;
+      vertical-align: middle;
+      width: 100%;
+      height: 100%;
+
+      img {
         max-width: 100%;
-        max-height: 100%;   
+        max-height: 100%;
         height: 100%;
-        object-fit: contain; 
-        
+        object-fit: contain;
+      }
     }
+  }
 }
 </style>
