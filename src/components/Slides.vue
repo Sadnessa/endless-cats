a7fa09c7bd949b7287755593b4de3369061d0262
Darya Volkova <anrich@bk.ru>
2022-01-14 03:16:19 +0200

    Added Slides component

---
+++ src/components/Slides.vue
@@ -0,0 +1,22 @@
+<template>
+     <div class="indicators">
+    <MyButton small></MyButton>
+  </div>
+</template>
+
+<script>
+import MyButton from "./MyButton.vue"
+
+export default {
+ components: { MyButton }
+}
+</script>
+
+<style lang="scss" scoped>
+.indicators {
+  position: absolute;
+  top: 85%;
+  width: 600px;
+  height: 35px;
+}
+</style>
