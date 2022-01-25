7b050f7b98c14eb753d14c7ef8c0f6d80e88a9f5
Darya Volkova <anrich@bk.ru>
2022-01-25 13:43:23 +0200

    Build v0.3

--- src/components/SliderCard.vue
+++ src/components/SliderCard.vue
@@ -4,10 +4,10 @@
       <div v-for="img in sliderImg" :key="img" class="img-wrapper">
         <div class="loading-wrapper" v-if="img == null">
           <div class="hollow-dots-spinner" :style="spinnerStyle">
-  <div class="dot"></div>
-  <div class="dot"></div>
-  <div class="dot"></div>
-</div>
+            <div class="dot"></div>
+            <div class="dot"></div>
+            <div class="dot"></div>
+          </div>
         </div>
         <img :src="img" v-else />
       </div>
@@ -48,7 +48,7 @@ export default {
     width: 100%;
     height: 100%;
     white-space: nowrap;
-    transition: all 0.5s ease-in-out; 
+    transition: all 0.5s ease-in-out;
 
     .img-wrapper {
       display: inline-flex;
@@ -68,46 +68,46 @@ export default {
   }
 }
 
-.hollow-dots-spinner, .hollow-dots-spinner * {
-      box-sizing: border-box;
-    }
+.hollow-dots-spinner,
+.hollow-dots-spinner * {
+  box-sizing: border-box;
+}
 
-    .hollow-dots-spinner {
-      height: 15px;
-      width: calc(30px * 3);
-    }
+.hollow-dots-spinner {
+  height: 15px;
+  width: calc(30px * 3);
+}
 
-    .hollow-dots-spinner .dot {
-      width: 15px;
-      height: 15px;
-      margin: 0 calc(15px / 2);
-      border: calc(15px / 5) solid #494949;
-      border-radius: 50%;
-      float: left;
-      transform: scale(0);
-      animation: hollow-dots-spinner-animation 1000ms ease infinite 0ms;
-    }
+.hollow-dots-spinner .dot {
+  width: 15px;
+  height: 15px;
+  margin: 0 calc(15px / 2);
+  border: calc(15px / 5) solid #494949;
+  border-radius: 50%;
+  float: left;
+  transform: scale(0);
+  animation: hollow-dots-spinner-animation 1000ms ease infinite 0ms;
+}
 
-    .hollow-dots-spinner .dot:nth-child(1) {
-      animation-delay: calc(300ms * 1);
-    }
+.hollow-dots-spinner .dot:nth-child(1) {
+  animation-delay: calc(300ms * 1);
+}
 
-    .hollow-dots-spinner .dot:nth-child(2) {
-      animation-delay: calc(300ms * 2);
-    }
+.hollow-dots-spinner .dot:nth-child(2) {
+  animation-delay: calc(300ms * 2);
+}
 
-    .hollow-dots-spinner .dot:nth-child(3) {
-      animation-delay: calc(300ms * 3);
+.hollow-dots-spinner .dot:nth-child(3) {
+  animation-delay: calc(300ms * 3);
+}
 
-    }
-
-    @keyframes hollow-dots-spinner-animation {
-      50% {
-        transform: scale(1);
-        opacity: 1;
-      }
-      100% {
-        opacity: 0;
-      }
-    }
+@keyframes hollow-dots-spinner-animation {
+  50% {
+    transform: scale(1);
+    opacity: 1;
+  }
+  100% {
+    opacity: 0;
+  }
+}
 </style>
